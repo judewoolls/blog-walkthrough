b565545e8024abf46a859239bac6cbc31baa69e1
Jude Woolls <jwoolls01@gmail.com>
2025-01-02 15:03:42 +0000

    check for error

--- codestar/settings.py
+++ codestar/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-4y@+jn(5sg@l8#qxds@h_qsc+tj&@tiu6#u(-#%d*!en&12my&'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['localhost', '.herokuapp.com']
 
