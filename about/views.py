f829664211482979b4f80942f58b8a048b48e3e9
Jude Woolls <jwoolls01@gmail.com>
2025-01-08 13:44:00 +0000

    add the about view and template and add the urls

--- about/views.py
+++ about/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, get_object_or_404
+from .models import About
 # Create your views here.
+
+def AboutDisplay(request):
+
+    about = About.objects.all().order_by("-updated_on").first()
+
+    return render(
+        request,
+        "about/about.html",
+        {"about": about},
+    )
